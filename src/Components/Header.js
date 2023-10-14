b6cbc7ecac7c9c23bbc2246af5a382ea314b1d7c
Dohyun Kim <56704790+kdhminime@users.noreply.github.com>
2023-10-14 16:40:13 -0600

    initial color

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,28 +1,30 @@
 import { Outlet, Link } from "react-router-dom";
+import "./Styles.css";
 
 const Header = () => {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/tournament-ranking">Tournament Ranking</Link>
-          </li>
-          <li>
-            <Link to="/team-ranking">Team Ranking</Link>
-          </li>
-          <li>
-            <Link to="/global-ranking">Global Ranking</Link>
-          </li>
-          <li>
-            <Link to="/predict-ranking">Predict Ranking</Link>
-          </li>
-        </ul>
-      </nav>
-
+      <div className="header">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/tournament-ranking">Tournament Ranking</Link>
+            </li>
+            <li>
+              <Link to="/team-ranking">Team Ranking</Link>
+            </li>
+            <li>
+              <Link to="/global-ranking">Global Ranking</Link>
+            </li>
+            <li>
+              <Link to="/predict-ranking">Predict Ranking</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
       <Outlet />
     </>
   );
