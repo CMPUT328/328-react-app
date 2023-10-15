2b52439d53a31e5408202c91ce645544e2fa15dd
adnanzaki19 <adnanzaki2@gmail.com>
2023-10-15 04:12:38 -0600

    home page done

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,9 +1,11 @@
 import '../Components/Styles.css';
 
-import HomeBgEffect from '../images/HomeBgEffect.svg'; 
-import Homebg from '../images/HomeBg.png'; // Adjust the path to where your image is located.
 import React, { useRef } from 'react';
 import '../Components/HomeScreen.css'; // Include styles
+
+import Homebg from '../images/HomeBg.png';
+import customScrollButton from '../images/diamond-icon.png';
+import { ReactComponent as DownArrow } from '../images/downArrow.svg';
 
 function HomeScreen() {
     const secondContainerRef = useRef(null);
@@ -17,9 +19,11 @@ function HomeScreen() {
             <div className="container" style={{ backgroundImage: `url(${Homebg})`, backgroundSize: 'cover' }}>
                 <div className="title-container">
                     <h1>Make your Worlds 2023 Prediction</h1>
-                    <button onClick={() => {/* Navigate to another screen */}}>Predict Ranking</button>
+                    <button className="custom-button" onClick={() => {/* Navigate to another screen */}}>
+                        Predict Ranking
+                    </button>
                 </div>
-                <button className="scroll-button" onClick={handleScroll}>Scroll Down</button>
+                <button className="scroll-button" onClick={handleScroll}></button>
             </div>
             
             <div className="container" ref={secondContainerRef}>
