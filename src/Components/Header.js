2e801aafcbc2f14cd7e727b7c1537346faa86fff
adnanzaki19 <adnanzaki2@gmail.com>
2023-10-15 02:59:47 -0600

    header complete

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,23 +2,25 @@
 import './Header.css';
 
 import TeamLogo from '../images/TeamLogo.png'; 
-
-import { NavLink } from "react-router-dom";
-
-import React from 'react';
+import { Link } from "react-router-dom";
+import React, { useState } from 'react';
 
 function Header() {
+    const [activeLink, setActiveLink] = useState("/");
+
     return (
         <header>
             <div className="logo-container">
-                <img src={ TeamLogo } alt="Logo" />
+                <Link to="/">
+                    <img src={ TeamLogo } alt="Logo" />
+                </Link>
             </div>
             <nav className="nav-container">
                 <ul>
-                    <li><NavLink to="/" activeClassName="activeLink">Home</NavLink></li>
-                    <li><NavLink to="/tournament-ranking" activeClassName="activeLink">Tournament Ranking</NavLink></li>
-                    <li><NavLink to="/team-ranking" activeClassName="activeLink">Team Ranking</NavLink></li>
-                    <li><NavLink to="/global-ranking" activeClassName="activeLink">Global Ranking</NavLink></li>
+                    <li><Link to="/" className={activeLink === "/" ? "activeLink" : ""} onClick={() => setActiveLink("/")}>Home</Link></li>
+                    <li><Link to="/tournament-ranking" className={activeLink === "/tournament-ranking" ? "activeLink" : ""} onClick={() => setActiveLink("/tournament-ranking")}>Tournament Ranking</Link></li>
+                    <li><Link to="/team-ranking" className={activeLink === "/team-ranking" ? "activeLink" : ""} onClick={() => setActiveLink("/team-ranking")}>Team Ranking</Link></li>
+                    <li><Link to="/global-ranking" className={activeLink === "/global-ranking" ? "activeLink" : ""} onClick={() => setActiveLink("/global-ranking")}>Global Ranking</Link></li>
                 </ul>
             </nav>
         </header>
