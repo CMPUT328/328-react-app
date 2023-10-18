5398ddf74cfc27eb63e04622fbcfd1f682050d12
Dohyun Kim <56704790+kdhminime@users.noreply.github.com>
2023-10-17 22:22:14 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-// import { Amplify, Auth } from "aws-amplify";
-// import config from "./aws-exports";
-// Amplify.configure(config);
+import { Amplify, Auth } from "aws-amplify";
+import config from "./aws-exports";
+Amplify.configure(config);
 
-// Auth.currentCredentials()
-//     .then((credentials) => {
-//         console.log("Guest credentials", credentials);
-//     })
-//     .catch((err) => console.error(err));
+Auth.currentCredentials()
+    .then((credentials) => {
+        console.log("Guest credentials", credentials);
+    })
+    .catch((err) => console.error(err));
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
