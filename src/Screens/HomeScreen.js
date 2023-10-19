0552d61b380778853e040c242177539b146c01c4
Dohyun Kim <56704790+kdhminime@users.noreply.github.com>
2023-10-19 00:29:43 -0600

    amplify avaliable again

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,5 +1,3 @@
-import "../Components/Styles.css";
-
 import React, { useRef } from "react";
 import "../Components/HomeScreen.css"; // Include styles
 import Homebg from "../images/HomeBg.png";
@@ -10,43 +8,40 @@ import { ReactComponent as DownArrow } from "../images/downArrow.svg";
 import GlobalRanking from "../Components/GlobalRanking/GlobalRanking";
 
 function HomeScreen() {
-    const secondContainerRef = useRef(null);
+  const secondContainerRef = useRef(null);
 
-    const handleScroll = () => {
-        secondContainerRef.current.scrollIntoView({ behavior: "smooth" });
-    };
+  const handleScroll = () => {
+    secondContainerRef.current.scrollIntoView({ behavior: "smooth" });
+  };
 
-    return (
-        <div className="home-container">
-            <div
-                className="container"
-                style={{
-                    backgroundImage: `url(${Homebg})`,
-                    backgroundSize: "cover",
-                }}
-            >
-                <div className="title-container">
-                    <h1>Make your Worlds 2023 Prediction</h1>
-                    <button
-                        className="custom-button"
-                        onClick={() => {
-                            /* Navigate to another screen */
-                        }}
-                    >
-                        Predict Ranking
-                    </button>
-                </div>
-                <button
-                    className="scroll-button"
-                    onClick={handleScroll}
-                ></button>
-            </div>
-
-            <div className="container" ref={secondContainerRef}>
-                <GlobalRanking />
-            </div>
+  return (
+    <div className="home-container">
+      <div
+        className="container"
+        style={{
+          backgroundImage: `url(${Homebg})`,
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="title-container">
+          <h1>Make your Worlds 2023 Prediction</h1>
+          <button
+            className="custom-button"
+            onClick={() => {
+              /* Navigate to another screen */
+            }}
+          >
+            Predict Ranking
+          </button>
         </div>
-    );
+        <button className="scroll-button" onClick={handleScroll}></button>
+      </div>
+
+      <div className="container" ref={secondContainerRef}>
+        <GlobalRanking />
+      </div>
+    </div>
+  );
 }
 
 export default HomeScreen;
