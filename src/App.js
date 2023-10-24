12cf2ee398b2ffae3747ea5f8ce00fb7a91238f8
Adnan Zaki <68474810+adnanzaki19@users.noreply.github.com>
2023-10-24 00:08:40 -0600

    global changed

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import TournamentRankingScreen from "./Screens/TournamentRankingScreen";
 import TeamRankingScreen from "./Screens/TeamRankingScreen";
 import GlobalRankingScreen from "./Screens/GlobalRankingScreen";
 import PredictRankingScreen from "./Screens/PredictRankingScreen";
-import PlayerRankingScreen from "./Screens/PlayerRankingScreen";
 
 const App = () => {
   return (
@@ -24,7 +23,6 @@ const App = () => {
         <Route path="/team-ranking" element={<TeamRankingScreen />} />
         <Route path="/global-ranking" element={<GlobalRankingScreen />} />
         <Route path="/predict-ranking" element={<PredictRankingScreen />} />
-        <Route path="/player-ranking" element={<PlayerRankingScreen />} />
       </Routes>
     </Router>
   );
